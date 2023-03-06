87b741398d76fd17aeadaa64776d4e3c72a30df6
Seunghyeong Lee <seunghyeong27@gmail.com>
2023-03-07 02:32:35 +0900

    Update :: Css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,5 @@ import "../styles/globals.css";
 export default function App({Component, pageProps}: AppProps) {
   return <Layout>
     <Component {...pageProps} />
-
-    <style jsx global>{`
-        p {
-          color: white;
-          background-color: grey;
-        }
-      `}</style>
   </Layout>;
 }
